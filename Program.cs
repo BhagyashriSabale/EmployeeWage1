65c14bafcf8899443e0b20479059ccc1ebd24022
BhagyashriSabale <balisabale16@gmail.com>
2023-06-28 13:51:27 +0530

    [Bhagyashri]Check Employee is Present or not

--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
+            int isPresent = 1;
+            Random random = new Random();
+            int employeeAttendence = random.Next(0, 2);
+            if (employeeAttendence == isPresent)
+            {
+                Console.WriteLine("employee attendence is present");
+            }
+            else
+            {
+                Console.WriteLine("employee attendence is absent");
+            }
         }
     }
 }
